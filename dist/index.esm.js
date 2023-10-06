1ddbdc7ff4f60d2a11f85493a4315b7df462cd3e
Alexander Mac <amatsibarov@gmail.com>
2023-10-06 17:46:11 +0300

    rewrite the build logic, integrate rollup and create bundles for different consumers

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -1,5 +1,4 @@
-"use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+var __createBinding = (undefined && undefined.__createBinding) || (Object.create ? (function(o, m, k, k2) {
     if (k2 === undefined) k2 = k;
     var desc = Object.getOwnPropertyDescriptor(m, k);
     if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
@@ -10,12 +9,12 @@ var __createBinding = (this && this.__createBinding) || (Object.create ? (functi
     if (k2 === undefined) k2 = k;
     o[k2] = m[k];
 }));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+var __setModuleDefault = (undefined && undefined.__setModuleDefault) || (Object.create ? (function(o, v) {
     Object.defineProperty(o, "default", { enumerable: true, value: v });
 }) : function(o, v) {
     o["default"] = v;
 });
-var __importStar = (this && this.__importStar) || function (mod) {
+var __importStar = (undefined && undefined.__importStar) || function (mod) {
     if (mod && mod.__esModule) return mod;
     var result = {};
     if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
---
+++ rollup.config.js
@@ -0,0 +1,32 @@
+const resolve = require('@rollup/plugin-node-resolve')
+const commonjs = require('@rollup/plugin-commonjs')
+const pkg = require('./package.json')
+
+module.exports = [
+  {
+    input: 'build/index.js',
+    output: {
+      name: 'o2diff',
+      file: pkg.browser,
+      format: 'umd',
+      globals: {
+        'lodash': '_'
+      }
+    },
+    plugins: [
+      resolve(),
+      commonjs()
+    ],
+    external: [
+      'lodash'
+    ]
+  },
+
+  {
+    input: 'build/index.js',
+    output: [
+      { file: pkg.main, format: 'cjs' },
+      { file: pkg.module, format: 'es' }
+    ]
+  }
+]
