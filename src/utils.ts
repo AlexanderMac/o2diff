02eacae7a732bdc8efeeccd1631055f964bbe88d
Alexander Mac <amatsibarov@gmail.com>
2025-01-28 11:22:13 +0300

    add getLibVersion function

--- src/utils.ts
+++ src/utils.ts
@@ -17,6 +17,10 @@ import {
 
 import { ArrayUnknown, Input, ObjectsDiff, RecordUnknown } from './types'
 
+export const getLibVersion = () => {
+  return '5.0.0'
+}
+
 export function getObjectPaths(input: Input, curPath = '', isArray = false): string[] {
   const paths: string[] = []
   forEach(input, (value, key) => {
